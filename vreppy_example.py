44b89744ad52af29be6d7d60428ef586b8996bb1
Chaitanya Perugu <33365011+chaitanyapb@users.noreply.github.com>
2017-11-06 21:48:51 -0500

    Uploaded vreppy.py and vreppy_example.py
    vreppy : A wrapper library for V-REP Python API
    vreppy_example : An example code for vreppy

---
+++ vreppy_example.py
@@ -0,0 +1,60 @@
+# Created on Mon Nov 06 21:16:22 2017
+# Author: Chaitanya Pb
+
+#%%
+import os
+import vreppy as vp
+
+#%%
+path_to_pxp = os.getcwd() + '/models/phantomXPincher.ttm'
+path_to_scene = os.getcwd() + '/scenes/emptyScene.ttt'
+
+joint_names = ['PhantomXPincher_joint1', 'PhantomXPincher_joint2', 
+               'PhantomXPincher_joint3', 'PhantomXPincher_joint4', 
+               'PhantomXPincher_gripperCenter_joint', 
+               'PhantomXPincher_gripperClose_joint']
+               
+#%%
+dt = 0.001
+stop_sim_time = 1
+
+sim_time = 0
+set_joint_val = 0
+dval = 0.003
+
+#%%
+vp.startVREP(port_num = 19997, wait = True)
+clientID = vp.connectToRemoteAPIServer(port_num = 19997, is_sync = True)
+
+vp.loadVREPScene(clientID, path_to_scene)
+
+pxp_base_handle = vp.loadVREPModel(clientID, path_to_pxp)
+xyz = vp.getAbsolutePosition(clientID, pxp_base_handle)
+position = (0, -0.5, xyz[2])
+orientation = (vp.d2r(90), vp.d2r(0), vp.d2r(90))
+vp.placeModelInScene(clientID, pxp_base_handle, position, orientation)
+
+joint_handles = vp.getAllJointHandles(clientID, joint_names)
+
+vp.setSimulationTimeStep(clientID, dt)
+vp.startSimulation(clientID)
+
+#%%
+while sim_time < stop_sim_time:
+    
+    print 't =', sim_time
+    
+    joint_handle = joint_handles[0]
+    
+    get_joint_val = vp.getJointPosition(clientID, joint_handle)
+    vp.setJointPosition(clientID, joint_handle, set_joint_val)
+    
+    sim_time += dt
+    set_joint_val += dval
+    print get_joint_val
+    
+    vp.syncSpinOnce(clientID)
+
+#%%
+vp.stopSimulation(clientID)
+vp.closeConnection(clientID)
